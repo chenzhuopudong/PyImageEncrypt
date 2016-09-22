73f62f9545d078187df02f375414ad94d641d38d
chenzhuo <chenzhuo.nsn@gmail.com>
2016-09-22 21:22:24 +0300

    day 1 backup

--- read_image.py
+++ read_image.py
@@ -7,5 +7,10 @@ def read_jpeg(file_name):
     jpgfile = Image.open(file_name)
 
     print("The details of",file_name)
+    jpgfile.show()
+    print (jpgfile.size)
+    #jpgfile.thumbnail(128, 128)
+    #thumbnail.show()
     print (jpgfile.bits, jpgfile.size, jpgfile.format, jpgfile.mode)
+
 
