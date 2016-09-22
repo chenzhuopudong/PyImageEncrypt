8fbd3bf75838801f21cd65c4746a1b9cb2b82fa4
chenzhuo <chenzhuo.nsn@gmail.com>
2016-09-22 20:29:20 +0300

    initial commit after file created

---
+++ main.py
@@ -0,0 +1,7 @@
+import os,sys
+from read_image import read_jpeg
+
+
+print("Read the file")
+read_jpeg()
+
---
+++ read_image.py
@@ -0,0 +1,5 @@
+import os,sys
+from PIL import Image
+
+def read_jpeg():
+    print("Read the image")
