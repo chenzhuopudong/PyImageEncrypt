9c1bd790529aa12012d6198b26c87c4e9b4f5cf3
chenzhuo <chenzhuo.nsn@gmail.com>
2016-09-22 20:42:13 +0300

    after file read

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from read_image import read_jpeg
 
 
 print("Read the file")
-read_jpeg()
+read_jpeg("C:/Users/yuanlili/Pictures/Saved Pictures/DSC05208.JPG")
 
--- read_image.py
+++ read_image.py
@@ -1,5 +1,11 @@
 import os,sys
 from PIL import Image
 
-def read_jpeg():
-    print("Read the image")
+def read_jpeg(file_name):
+
+    print("Read the image",file_name)
+    jpgfile = Image.open(file_name)
+
+    print("The details of",file_name)
+    print (jpgfile.bits, jpgfile.size, jpgfile.format, jpgfile.mode)
+
