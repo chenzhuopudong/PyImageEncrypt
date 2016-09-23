33eeaa2634b91ff47e20a181f622fca9aea56f02
chenzhuo <chenzhuo.nsn@gmail.com>
2016-09-23 21:59:07 +0300

    day 2 backup

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import os,sys
 from read_image import read_jpeg,blur_jpge,thumbnail_jpge
 
-resource_path = 'C:/Users/yuanlili/PycharmProjects/PyImageEncrypt/resource/'
-jpg_name = 'cz_001.jpg'
-
-print("Read the file")
+resource_path = 'C:/tmp/resource/'
+jpg_name = 'tmp_001.jpg'
 
 #tested suits
 #read_jpeg(resource_path+jpg_name)
 #blur_jpge(resource_path+jpg_name)
-
-thumbnail_jpge(resource_path+jpg_name)
+#thumbnail_jpge(resource_path+jpg_name)
