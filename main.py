cfe97352a83cd6199abd2caa864ab21768ad78ed
chenzhuo <chenzhuo.nsn@gmail.com>
2016-09-28 21:20:13 +0300

    day5 backup

--- main.py
+++ main.py
@@ -15,11 +15,35 @@ class PNGimage:
 
     new_data = []
 
-    def __init__(self,src_file):
+    def __init__(self,src_file,result_file):
         self.Im = Image.open(src_file)
         self.Im = self.Im.convert("RGBA")
         self.datas = self.Im.getdata()
+        self.result_file = result_file
 
     def clean_LSB(self):
-        for item in datas:
-            new_data.append((item[0] & 254, item[1] & 254, item[2] & 254, item[3] & 254))
+        for item in self.datas:
+            self.new_data.append((item[0] & 254, item[1] & 254, item[2] & 254, item[3] & 254))
+
+    def save_result(self):
+        self.Im.putdata(self.new_data)
+        self.Im.save(self.result_file, "PNG")
+
+class InfoBody:
+
+    file_folder='C:/tmp/resource/'
+
+    def __init__(self,info):
+        self.hex_for_file = bytes(info.encode())
+
+    def show_hex(self):
+        print(self.hex_for_file)
+
+    def test_data(self,output_file):
+        self.output_file = open(self.file_folder+output_file, 'wb')
+        self.output_file.write(self.hex_for_file)
+        self.output_file.close()
+
+tmp_info = InfoBody('abc我吃了defe511656很多')
+tmp_info.test_data('tmp003.bin')
+tmp_info.show_hex()
