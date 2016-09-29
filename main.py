aa12250febb23891204b7321e7f5349245cf6ddc
chenzhuo <chenzhuo.nsn@gmail.com>
2016-09-29 22:00:45 +0300

    day6 backup

--- main.py
+++ main.py
@@ -11,7 +11,15 @@ run_text_test()
 # 3 get the length of the array of byte
 # 4 Use 'or' to embedded the data into LSB
 
+#note1, at first we needn't encrypt, and we don't need to have length at all, we just put
+#data from the begining. And when we try to extract information, the data end can be found when
+#all data starts to be 0.
+
+#note2, at step 2 we can make all data with 'hole', like interweave, and keep all the other data unchanged
+
+
 class PNGimage:
+
 
     new_data = []
 
@@ -37,12 +45,18 @@ class InfoBody:
         self.hex_for_file = bytes(info.encode())
 
     def show_hex(self):
+        print('length of data is:',len(self.hex_for_file))
         print(self.hex_for_file)
+        for i in range (len(self.hex_for_file)):
+            print(bin(self.hex_for_file[i]))
 
     def test_data(self,output_file):
         self.output_file = open(self.file_folder+output_file, 'wb')
         self.output_file.write(self.hex_for_file)
         self.output_file.close()
+
+    def byte_to_4bit_tuple(self):
+
 
 tmp_info = InfoBody('abc我吃了defe511656很多')
 tmp_info.test_data('tmp003.bin')
